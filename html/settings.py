347af28c9aa4f2a5575c1e771293c1246f2b4ad3
Saff <djsaff@gmail.com>
2011-10-27 16:19:13 +0400

    bugs

--- html/settings.py
+++ html/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'unshort',                      # Or path to database file if using sqlite3.
         'USER': 'unshort',                      # Not used with sqlite3.
-        'PASSWORD': 'asdcf3wc234',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'mysql1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
